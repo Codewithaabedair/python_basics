b1eee0fc90040febc0a71f3f6a504607ab69210c
Codewithaabedair <aabedairyt@gmail.com>
2026-03-18 16:19:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = input("What is your name?")
-
-age = input("How old are you?")
-
-print("Hello " + name + " you are " + age + " years old")
+name = input("What is your name? ")
+# This stores awnser into a variable
+age = input("How old are you? ")
+# This stores awnser into a variable
+print(f"Hello {name}, you are {age} years old")
+# This show text
